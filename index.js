9d2e259c66564dbaff1968cbf8de6ee172067a65
kizwolak <ki.zwolak@gmail.com>
2023-04-01 01:05:00 -0300

    Create GET request and server

---
+++ index.js
@@ -0,0 +1,36 @@
+const http = require("http");
+const https = require("https");
+
+const hostname = "127.0.0.1";
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World\n");
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+const options = {
+  hostname: "example.com",
+  port: 443,
+  path: "/todos",
+  method: "GET",
+};
+
+const req = https.request(options, (res) => {
+  console.log(`statusCode: ${res.statusCode}`);
+
+  res.on("data", (d) => {
+    process.stdout.write(d);
+  });
+
+  req.on("error", (error) => {
+    console.error(error);
+  });
+});
+
+req.end();
